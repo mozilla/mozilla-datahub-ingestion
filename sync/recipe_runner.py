ad4736cf0103fdf406a6d64906dde5eb2342b488
Alexander Nicholson <anicholson@mozilla.com>
2022-08-31 09:38:56 -0400

    Update CLI to forward to DataHub CLI instead of using library

--- sync/recipe_runner.py
+++ sync/recipe_runner.py
@@ -1,10 +1,10 @@
 import logging
 import subprocess
+import tempfile
 from datetime import datetime
 
 from pathlib import Path
 import yaml
-from datahub.ingestion.run.pipeline import Pipeline
 from jinja2 import Environment, FileSystemLoader
 
 LOOKML_REPOSITORY_CONFIG = "lookml_repositories.yaml"
@@ -53,7 +53,6 @@ def _generate_lookml_recipes(recipes_path: Path) -> None:
         )
 
         # 2. Dynamically build the recipe (some are private)
-
         path_to_lookml_project = Path(f"./lookml_repositories/{lookml_project}")
         with open(recipes_path / f"{lookml_project}.dhub.yaml", "w") as f:
             f.write(
@@ -75,6 +74,18 @@ def _replace_sink_with_static_file(recipe_filename: str) -> dict:
 
 
 def run_recipes_in_dir(dirname: str, dump_to_file: bool) -> None:
+    """
+    This function is a thin wrapper around the DataHub CLI ingest method with some additional functionality
+    for debugging and generating recipes.
+
+    We forward to the DataHub CLI instead of using DataHub as a library (via datahub.ingestion.run.pipeline) because:
+       1. It has additional logging/reporting.
+       2. It expands environment variables in recipe files.
+
+    Forwarding/invoking to the datahub.cli Click functions was also considered, but that has custom configuration
+    that I didn't figure out how to forward.
+    """
+
     recipes_path = Path(dirname)
 
     if recipes_path.name == "lookml":
@@ -94,4 +105,9 @@ def run_recipes_in_dir(dirname: str, dump_to_file: bool) -> None:
         if dump_to_file:
             config["sink"] = _replace_sink_with_static_file(recipe_file.name)
 
-        Pipeline.create(config).run()
+            with tempfile.NamedTemporaryFile("w", suffix=".dhub.yaml") as new_recipe:
+                yaml.dump(config, new_recipe)
+                subprocess.call(["datahub", "ingest", "-c", new_recipe.name])
+
+        else:
+            subprocess.call(["datahub", "ingest", "-c", str(recipe_file)])
