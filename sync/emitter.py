263668c81ba491f879fd77e5deb6a721423f512a
Alexander Nicholson <anicholson@mozilla.com>
2022-09-21 15:00:18 -0400

    Chore: Delete old glean lineage function

--- sync/emitter.py
+++ sync/emitter.py
@@ -113,30 +113,6 @@ def legacy_telemetry_emitter(dump_to_file: bool = False):
     log.info("Running the Legacy Telemetry emitter")
 
 
-def bigquery_glean_lineage_emitter(dump_to_file: bool = False):
-    log.info("Running the BigQuery Glean Lineage emitter")
-
-    lineage_mce = builder.make_lineage_mce(
-        upstream_urns=[
-            builder.make_dataset_urn("Glean", "test-app.main"),
-        ],
-        downstream_urn=builder.make_dataset_urn(
-            "bigquery", "anich-sandbox.ping_live.test_app_main"
-        ),
-    )
-
-    if dump_to_file:
-        static_filename = f"bigquery_glean_lineage-{datetime.now()}.test.json"
-        with open(static_filename, "w") as f:
-            json.dump(lineage_mce.to_obj(), f, indent=4)
-        return
-
-    emitter = DataHubRestEmitter(
-        gms_server=os.environ["DATAHUB_GMS_URL"], token=os.environ["DATAHUB_GMS_TOKEN"]
-    )
-    emitter.emit_mce(lineage_mce)
-
-
 def bigquery_legacy_telemetry_lineage_emitter(dump_to_file: bool = False):
     log.info("Running the BigQuery Legacy Telemetry Lineage Emitter")
 
@@ -144,7 +120,6 @@ def bigquery_legacy_telemetry_lineage_emitter(dump_to_file: bool = False):
 EMITTERS = {
     "glean": glean_emitter,
     "legacy_telemetry": legacy_telemetry_emitter,
-    "bigquery_glean_lineage": bigquery_glean_lineage_emitter,
     "bigquery_legacy_lineage": bigquery_legacy_telemetry_lineage_emitter,
 }
 
