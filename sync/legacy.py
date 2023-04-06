9cba47616e562ba6467b4f913d8326dc81cae6b5
Linh Nguyen <linhnguyen@mozilla.com>
2023-04-06 13:24:01 -0400

    Address review sans test

--- sync/legacy.py
+++ sync/legacy.py
@@ -1,6 +1,6 @@
 from collections import defaultdict
 from dataclasses import dataclass
-from typing import Sequence
+from typing import Sequence, Dict
 
 import requests
 import tarfile
@@ -27,13 +27,22 @@ class LegacyPing:
         return table_names
 
 
-def get_ping_schemas():
+def _get_ping_schemas() -> Dict[str, str]:
+    """
+    Fetches the latest version of the schema tarball from GitHub and returns a dict of ping names and their schema versions.
+
+    :return: Dict[str, str]
+    Examples:
+    {'account-ecosystem': ['account-ecosystem.4.schema.json'], 'android-anr-report': ['android-anr-report.1.schema.json', 'android-anr-report.2.schema.json'], 'anonymous': ['anonymous.4.schema.json']}
+    """
+    print("Fetching schemas from GitHub...")
     schemas = requests.get(SCHEMA_URL)
     schema_file = BytesIO(schemas.content)
 
     with tarfile.open(fileobj=schema_file, mode="r|gz") as tar:
         schema_versions = defaultdict(list)
         for member in tar:
+            # pdb.set_trace()
             if member.name.startswith(
                 "mozilla-pipeline-schemas-generated-schemas/schemas/telemetry/"
             ) and member.name.endswith(".schema.json"):
@@ -44,7 +53,7 @@ def get_ping_schemas():
 
 def get_legacy_pings() -> Sequence[LegacyPing]:
     pings = []
-    schema_versions = get_ping_schemas()
+    schema_versions = _get_ping_schemas()
     for key, value in schema_versions.items():
         pings.append(
             LegacyPing(
