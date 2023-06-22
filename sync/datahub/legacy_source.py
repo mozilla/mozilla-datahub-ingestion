c0cfa16b3dd03e6f556e8fbf3ae55bfa3f2bb9b5
Alexander Nicholson <anicholson@mozilla.com>
2023-06-22 10:07:29 -0400

    Fix imports after datahub upgrade

--- sync/datahub/legacy_source.py
+++ sync/datahub/legacy_source.py
@@ -20,7 +20,7 @@ from datahub.ingestion.source.state.stateful_ingestion_base import (
     StatefulIngestionConfigBase,
     StatefulIngestionSourceBase,
 )
-from datahub.utilities.source_helpers import (
+from datahub.ingestion.api.source_helpers import (
     auto_stale_entity_removal,
     auto_workunit_reporter,
 )
